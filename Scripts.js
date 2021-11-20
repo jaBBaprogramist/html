664c19b8443f4f854c3e14234e5b76c22e7028ee
jaBBaprogramist <lexaserious@gmai.com>
2021-11-20 14:35:44 +0700

    commit

--- Scripts.js
+++ Scripts.js
@@ -1,17 +1,23 @@
 
 document.addEventListener("DOMContentLoaded",() => {
+    const form = document.getElementById("form");
 
-    const elementbuttonadd = document.getElementById("add");
-    const elementlist = document.getElementById("list");
-    let list = [];
-    let i = 0;
-    const elementinput = document.getElementById("input");
-    elementbuttonadd.addEventListener("click",() => {
-        i++;
-        document.getElementById("list").innerHTML = elementlist.innerHTML + "<li id = i onclick='del()'>" + document.getElementById("input").value +"<button style=\"width: 50px\">&#128298;</button>" + "</li>";
-        list.push(i);
+    form.addEventListener("submit",event => {
+        event.preventDefault()
+        const elementbuttonadd = document.getElementById("add");
+        const elementlist = document.getElementById("list");
+        let list = [];
+        let i = 0;
+        const elementinput = document.getElementById("input");
+        elementbuttonadd.addEventListener("click",() => {
+            i++;
+            elementlist.innerHTML = elementlist.innerHTML + `<li id='${i+1}' >` + elementinput.value +"<button style=\"width: 50px\" onclick='del("+(i+1)+")'>&#128298;</button>" + "</li>";
+            list.push(i);
+        })
+
     })
 })
-function del(){
-
+function del(id){
+    const elementdel = document.getElementById(id);
+    elementdel.parentNode.removeChild(elementdel);
 }
