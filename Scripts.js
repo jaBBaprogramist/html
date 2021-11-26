4ba5d49528b870a919a7bbd52a15e746eeebaf8e
JABBAprogrammist <lexa0192837465>
2021-11-26 14:42:14 +0700

    commit

--- Scripts.js
+++ Scripts.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded",() => {
         const elementinput = document.getElementById("input");
         elementbuttonadd.addEventListener("click",() => {
             i++;
-            elementlist.innerHTML = elementlist.innerHTML + `<li id='${i+1}' >` + elementinput.value +"<button style=\"width: 50px\" onclick='del("+(i+1)+")'>&#128298;</button>" + "</li>";
+            elementlist.innerHTML = elementlist.innerHTML + `<li id='${i}' >`+`<input id='C${i}' onclick="updateItem(${i})" type="checkbox" >` + elementinput.value + '<button  style="width: 50px" onclick="del("+(i+1)+")">&#128298;</button>' + "</li>";
             list.push(i);
         })
 
@@ -20,4 +20,9 @@ document.addEventListener("DOMContentLoaded",() => {
 function del(id){
     const elementdel = document.getElementById(id);
     elementdel.parentNode.removeChild(elementdel);
+}
+function updateItem(id) {
+    if (document.getElementById("C" + id).checked) {
+        document.getElementById(id).style.textDecoration = "line-through";
+    }
 }
